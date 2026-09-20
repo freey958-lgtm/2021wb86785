00ef7c8cdfb7e0e00ec7d689e57fa097bf9defa4
Rahul Saw <freey958@gmail.com>
2026-09-20 14:56:09 +0530

    Modify App for continuous integration

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Lab");
+        System.out.println("Hello from DevOps Lab - Continuous Integration");
     }
 }
